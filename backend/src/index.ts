0aab21bd51f6600dd8e540841ab3c21defbab3c8
maaret <maaret.lyytinen@gmail.com>
2023-08-14 13:12:40 +0300

    add front and back base

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express"
+
+const server = express()
+
+server.use("/", express.static("./dist/frontend"))
+
+server.use("/version", (req: Request,res: Response) => {
+	res.send("1.0")
+})
+
+server.get("/", (_req: Request, res: Response) => {
+	res.send("Hello keskuskauppa!")
+})
+
+server.listen(3000, () => {
+	console.log("Listening to port 3000")
+})
