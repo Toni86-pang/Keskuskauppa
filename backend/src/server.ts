34ca3adea6aed62789d0e5e23f915499e6a835ee
Joonas Huhdanmäki <jhuhdanmaki@gmail.com>
2023-08-15 14:11:47 +0300

    version 1.26

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,7 +18,7 @@ server.use("/api/users", users)
 server.use("/", express.static("./dist/frontend"))
 
 server.use("/version", (req: Request, res: Response) => {
-	res.send("1.261")
+	res.send("1.262")
 })
 
 server.get("/", (_req: Request, res: Response) => {
