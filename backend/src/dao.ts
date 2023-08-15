73b051c55b93a82c066d967ac51bd323adb30f8e
maaret <maaret.lyytinen@gmail.com>
2023-08-15 14:37:23 +0300

    Add user registration.

--- backend/src/dao.ts
+++
@@ -1,8 +0,0 @@
-import { executeQuery } from "./database"
-
-
-export const getAllUsers = async () => {
-	const query = "SELECT * FROM users"
-	const result = await executeQuery(query)
-	return result
-}
