00bb9d744f6032b4e08dfb64b7d9cc625bb54b4b
Joonas Huhdanmäki <jhuhdanmaki@gmail.com>
2023-08-15 09:00:24 +0300

    version 1.23

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ const server = express()
 server.use("/", express.static("./dist/frontend"))
 
 server.use("/version", (req: Request, res: Response) => {
-	res.send("1.22")
+	res.send("1.23")
 })
 
 server.get("/", (_req: Request, res: Response) => {
