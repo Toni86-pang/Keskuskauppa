8ee675cb4c0edef5770e3d1cac2a670624f838a8
Joonas Huhdanmäki <jhuhdanmaki@gmail.com>
2023-08-16 12:52:23 +0300

    Added delete user endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,20 @@
 import express, { Request, Response } from "express"
-import {
-	createUsersTable,
-	createGategoryTable,
-	createSubcategoryTable,
-	createProductsTable,
-	createSalesTable,
-	createReviewsTable,
-	createMessageLogTable
-}
-	from "./database"
+// import {
+// 	createUsersTable,
+// 	createGategoryTable,
+// 	createSubcategoryTable,
+// 	createProductsTable,
+// 	createSalesTable,
+// 	createReviewsTable,
+// 	createMessageLogTable
+// }
+// 	from "./database"
 import users from "./routers/usersRouters"
 
 const server = express()
 server.use(express.json())
 server.use("/api/users", users)
-
 server.use("/", express.static("./dist/frontend"))
-
 server.use("/version", (req: Request, res: Response) => {
 	res.send("1.26344")
 })
@@ -25,12 +23,12 @@ server.get("/", (_req: Request, res: Response) => {
 	res.send("Hello keskuskauppa!")
 })
 
-createUsersTable()
-createGategoryTable()
-createSubcategoryTable()
-createProductsTable()
-createSalesTable()
-createReviewsTable()
-createMessageLogTable()
+// createUsersTable()
+// createGategoryTable()
+// createSubcategoryTable()
+// createProductsTable()
+// createSalesTable()
+// createReviewsTable()
+// createMessageLogTable()
 
 export default server
