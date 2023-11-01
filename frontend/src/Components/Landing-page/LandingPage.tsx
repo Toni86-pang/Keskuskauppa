593d82262fb21073d6ebe3167892e42e5bd9fa29
Toni86-pang <neworen@gmail.com>
2023-11-01 09:36:39 +0200

    app.tsx should be done

--- frontend/src/Components/Landing-page/LandingPage.tsx
+++ frontend/src/Components/Landing-page/LandingPage.tsx
@@ -42,14 +42,10 @@ const LandingPage = () => {
 		"0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)",
 		},
 	}
-	
-	
+		
 	const [token] = useContext(UserTokenContext)
 	const [user, setUser] = useState<User | null>(null)
 	const isLoggedIn = Boolean(user)
-
-
-
 
 	useEffect(() => {
 		const fetchUserDetails = async () => {
