f4cf98f940bf0ad4b581c682617f19d1ad787d71
Toni86-pang <neworen@gmail.com>
2023-11-07 12:02:16 +0200

    fixed styling

--- frontend/src/Components/Landing-page/Carousel/Carousel.tsx
+++ frontend/src/Components/Landing-page/Carousel/Carousel.tsx
@@ -1,35 +1,29 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import "react-responsive-carousel/lib/styles/carousel.css"
 import { Carousel } from "react-responsive-carousel"
-import { makeStyles } from "@mui/material/styles"
+import { Box, styled } from "@mui/system"
 import { ProductType } from "../../../Services-types/types"
 import { ReactNode } from "react"
 import { useNavigate } from "react-router"
-import { Card, CardContent, CardMedia, Paper, Typography } from "@mui/material"
+import { Card, CardMedia, Paper, Typography } from "@mui/material"
 
 interface CarouselProducts {
 	carouselProducts: ProductType[]
 }
 
 function CustomCarousel({ carouselProducts }: CarouselProducts) {
-	const useStyles = makeStyles({
-		carouselContainer: {
-			maxWidth: "1920px",
-			minWidth: "200px",
-			maxHeight: "200px",
-			width: "auto",
-			height: "auto",
-			objectFit: "contain",
-			margin: "0px auto",
-			fontFamily: "arial",
-		},
-		responsiveImage: {
-			aspectRatio: "0",
-			objectFit: "contain",
-		},
-	})
-
-	const classes = useStyles()
+	const CustomPaper = styled(Paper)`
+	max-width: 100%; 
+	margin: 0 auto;
+	background: "#f3f6fa",
+  `
+  
+	const CustomCard = styled(Card)`
+	margin-right: 300px;
+	margin-left: 300px;
+  `  
+	
+	
 	const navigate = useNavigate()
 
 	const handleClick = (_index: number, item: ReactNode) => {
@@ -39,8 +33,7 @@ function CustomCarousel({ carouselProducts }: CarouselProducts) {
 	}
 
 	return (
-		<Paper sx={classes.carouselcontainer}>
-
+		<CustomPaper>
 			<Carousel
 				onClickItem={handleClick}
 				showThumbs={false}
@@ -48,26 +41,38 @@ function CustomCarousel({ carouselProducts }: CarouselProducts) {
 				autoPlay={true}
 				interval={1500}
 				infiniteLoop={true}
+				
 			>
-
 				{carouselProducts.map((product, index) => {
 					return (
-						<Card key={"Carousel " + index} data-productid={product.product_id} className="carousel-slide">
-							<CardMedia 
-								component="img" 
-								sx={classes.responsiveimage}
-								src={product.product_image} 
-								alt={product.title} />
-
-							<CardContent>
-								<Typography variant="h3">{product.title}</Typography>
-								<Typography>Hinta: {product.price}</Typography>
-							</CardContent>
-						</Card>
+						
+						<CustomCard key={"Carousel " + index} data-productid={product.product_id} >
+							<Box
+								style={{
+									overflow: "hidden", 
+								}}
+							>
+								<CardMedia
+									component="img"
+									src={product.product_image}
+									alt={product.title}
+									sx={{
+										maxHeight: "300px",
+										height: "auto", 
+										maxWidth: "100%",
+										objectFit: "cover",
+										background: "#f3f6fa",
+									}}
+								/>
+							</Box>
+							<Typography variant="h4">{product.title}</Typography>
+							<Typography>Hinta: {product.price}</Typography>
+						</CustomCard>
+						
 					)
 				})}
-			</Carousel >
-		</Paper>
+			</Carousel>
+		</CustomPaper>
 	)
 }
 
